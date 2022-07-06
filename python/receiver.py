51c4fb7ffe0bec0c9715ff1383f7a569cbd75c30
Lukasz Kulacz <lukasz.kulacz@rimedolabs.com>
2022-07-06 14:53:27 +0200

    template for transmiter

--- python/receiver.py
+++ python/receiver.py
@@ -29,16 +29,6 @@ class receiver(gr.basic_block):
     def __init__(self):
         gr.basic_block.__init__(self,
             name="receiver",
-            in_sig=[<+numpy.float32+>],
-            out_sig=[<+numpy.float32+>])
+            in_sig=[],
+            out_sig=[])
 
-    def forecast(self, noutput_items, ninput_items_required):
-        #setup size of input_items[i] for work call
-        for i in range(len(ninput_items_required)):
-            ninput_items_required[i] = noutput_items
-
-    def general_work(self, input_items, output_items):
-        output_items[0][:] = input_items[0]
-        consume(0, len(input_items[0]))
-        #self.consume_each(len(input_items[0]))
-        return len(output_items[0])
--- python/transmitter.py
+++ python/transmitter.py
@@ -22,23 +22,74 @@
 import numpy
 from gnuradio import gr
 
+import serial
+import pynmea2
+import requests
+from threading import Thread
+
+
+gps_value = {
+	'timestamp' : '',
+	'latitude': 0.0,
+	'longitude': 0.0,
+	'speed': 0.0,
+	'bearing': 0.0
+}
+
+def read_gps(device_name):
+	global gps_value
+	with serial.Serial(device_name) as ser:
+		while True:
+			line = ser.readline().decode('ascii', errors='replace')
+			nmeaobj = pynmea2.parse(line)
+			x = ['%s: %s' % (nmeaobj.fields[i][0], nmeaobj.data[i]) for i in range(len(nmeaobj.fields))]
+			#print(dir(nmeaobj))
+			#print(x)
+			#print(nmeaobj.fields)
+			#print('new gps value')
+			#print(type(nmeaobj.timestamp))
+			#print(nmeaobj.timestamp, nmeaobj.latitude, nmeaobj.longitude)
+			try:
+				gps_value['timestamp'] = nmeaobj.timestamp.strftime("%H:%M:%S.%f")
+				gps_value['latitude'] = nmeaobj.latitude
+				gps_value['longitude'] = nmeaobj.longitude
+				gps_value['speed'] = nmeaobj.speed #nmeaobj.spd_over_grnd
+				gps_value['bearing'] = nmeaobj.true_course if nmeaobj.true_course != None else 0.0
+			except:
+				pass
+
+
 class transmitter(gr.basic_block):
-    """
-    docstring for block transmitter
-    """
-    def __init__(self):
-        gr.basic_block.__init__(self,
-            name="transmitter",
-            in_sig=[<+numpy.float32+>],
-            out_sig=[<+numpy.float32+>])
+	def __init__(self, url, device_name, platoon_id):
+		gr.basic_block.__init__(self,
+			name="transmitter",
+			in_sig=[],
+			out_sig=[])
 
-    def forecast(self, noutput_items, ninput_items_required):
-        #setup size of input_items[i] for work call
-        for i in range(len(ninput_items_required)):
-            ninput_items_required[i] = noutput_items
+		self.device_name = device_name
+		self.url = url
+		self.platoon_id = platoon_id
 
-    def general_work(self, input_items, output_items):
-        output_items[0][:] = input_items[0]
-        consume(0, len(input_items[0]))
-        #self.consume_each(len(input_items[0]))
-        return len(output_items[0])
+		new_thread = Thread(target=read_gps, args=(self.device_name,))
+		new_thread.start()
+
+
+	def communicate(self):
+		url = self.url + '/' + str(self.platoon_id) 
+		data = {
+			  "key": "not_used_now",
+			  "lat": gps_value['latitude'],
+			  "lon": gps_value['longitude'],
+			  "speed": gps_value['speed'],
+			  "azim": gps_value['bearing'],
+			  "delay": 0.5
+		}
+
+
+		x = requests.post(url, json = data)
+
+		print(x.json())
+
+		return x.json()['freq']
+	
+
